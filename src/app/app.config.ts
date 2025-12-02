8e6c4c3c4140eb06ff9a4ee06624d4578830b7dc
Paul Gross <p.gross6408@gmail.com>
2025-12-02 09:46:03 +0100

    refactor: app configuration and enhance NoteListService with Firestore integration.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,5 +6,5 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"danotes-36412","appId":"1:1090523998352:web:094ab390e93cc81a541015","storageBucket":"danotes-36412.firebasestorage.app","apiKey":"AIzaSyDOkkiqbTp7ACudmhFMkEwntJu-oFzxJHs","authDomain":"danotes-36412.firebaseapp.com","messagingSenderId":"1090523998352","projectNumber":"1090523998352","version":"2"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"danotes-36412","appId":"1:1090523998352:web:094ab390e93cc81a541015","storageBucket":"danotes-36412.firebasestorage.app","apiKey":"AIzaSyDOkkiqbTp7ACudmhFMkEwntJu-oFzxJHs","authDomain":"danotes-36412.firebaseapp.com","messagingSenderId":"1090523998352"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import { collection, doc, Firestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NoteListService {
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
 
-  constructor() { }
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {}
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingleNoteRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
 }
