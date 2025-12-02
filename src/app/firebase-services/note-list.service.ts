8e3ab3c68f53d13d6d7897df8b25fd835c12153a
Paul Gross <p.gross6408@gmail.com>
2025-12-02 10:48:33 +0100

    feat: display notes and trash lists from firestore

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { Note } from '../interfaces/note.interface';
-import { collection, doc, Firestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { collection, doc, Firestore, collectionData, onSnapshot } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
@@ -10,9 +9,48 @@ export class NoteListService {
   trashNotes: Note[] = [];
   normalNotes: Note[] = [];
 
+  unsubTrash;
+  unsubNotes;
+
   firestore: Firestore = inject(Firestore);
 
-  constructor() {}
+  constructor() {
+    this.unsubTrash = this.subTrashList();
+    this.unsubNotes = this.subNotesList();
+  }
+
+  ngOnDestroy(){
+    this.unsubTrash();
+    this.unsubNotes();
+  }
+
+  subTrashList(){
+    return onSnapshot(this.getTrashRef(), (list) => {
+      this.trashNotes = [];
+      list.forEach(element => {
+        this.trashNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+  subNotesList(){
+    return onSnapshot(this.getNotesRef(), (list) => {
+      this.normalNotes = [];
+      list.forEach(element => {
+        this.normalNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+  setNoteObject(obj: any, id: string): Note {
+    return {
+      id: id ,
+      type: obj.type || 'note',
+      title: obj.title || '',
+      content: obj.content || '',
+      marked: obj.marked || false,
+    };
+  }
 
   getNotesRef() {
     return collection(this.firestore, 'notes');
--- src/app/note-list/note-list.component.ts
+++ src/app/note-list/note-list.component.ts
@@ -5,77 +5,45 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NoteComponent } from './note/note.component';
 
-
-
 @Component({
   selector: 'app-note-list',
   standalone: true,
   imports: [FormsModule, CommonModule, NoteComponent],
   templateUrl: './note-list.component.html',
-  styleUrl: './note-list.component.scss'
+  styleUrl: './note-list.component.scss',
 })
 export class NoteListComponent {
-  noteList: Note[] = [];
-  favFilter: "all" | "fav" = "all";
-  status: "notes" | "trash" = "notes";
+  favFilter: 'all' | 'fav' = 'all';
+  status: 'notes' | 'trash' = 'notes';
 
-  constructor(public noteService: NoteListService) {
-    this.noteList = this.getDummyData()
+  constructor(private noteService: NoteListService) {
   }
 
-  changeFavFilter(filter:"all" | "fav"){
+  getList(): Note[] {
+    // zuerst je nach Status (notes oder trash) die richtige Liste holen
+    const baseList =
+      this.status === 'notes'
+        ? this.noteService.normalNotes
+        : this.noteService.trashNotes;
+
+    // dann optional nach Favoriten filtern
+    if (this.favFilter === 'fav') {
+      return baseList.filter((note) => note.marked);
+    }
+
+    return baseList;
+  }
+
+  changeFavFilter(filter: 'all' | 'fav') {
     this.favFilter = filter;
   }
 
-  changeTrashStatus(){
-    if(this.status == "trash"){
-      this.status = "notes";
+  changeTrashStatus() {
+    if (this.status == 'trash') {
+      this.status = 'notes';
     } else {
-      this.status = "trash";
-      this.favFilter = "all";
+      this.status = 'trash';
+      this.favFilter = 'all';
     }
   }
-
-
-
-
-  getDummyData(): Note[] {
-    return [
-      {
-        id: "21sasd561dd4sdf",
-        type: "note",
-        title: "Block, Inline, and Inline-Block",
-        content: "https://www.youtube.com/watch?v=x_i2gga-sYg",
-        marked: true,
-      },
-      {
-        id: "25sd4f561w54sdf",
-        type: "note",
-        title: "css selector",
-        content: `kind p > b   (direktes kind) 
-        nachfahren p b  (alle nachfahren)
-        geschwister p ~ b (auf gleicher ebene ist VOR dem p ein b)`,
-        marked: true,
-      },
-      {
-        id: "54a4s6d546ff",
-        type: "note",
-        title: "aufräumen",
-        content: "Wohnzimmer saugen",
-        marked: false,
-      },
-      {
-        id: "2a35s4d654a6s4d",
-        type: "note",
-        title: "links",
-        content: `Reihenfolge: a:visited 
-        a:focus 
-        a:hover 
-        a:active
-        merkspruch: LoVe HAte`,
-        marked: true,
-      }
-    ];
-  }
-
 }
